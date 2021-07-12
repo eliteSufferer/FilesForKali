961f22076a01027868663efb17f545d7267d6a88
sergeyprohaker <46615564+sergeyprohaker@users.noreply.github.com>
2021-07-12 17:59:11 +0700

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 import subprocess
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 11:22:33:77:88:55", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+
